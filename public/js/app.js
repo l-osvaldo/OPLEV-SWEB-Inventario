c49b40d931f9f16b15f269ed25a40f90bf131286
francisco javier <javier24viper@gmail.com>
2019-04-25 19:57:10 +0000

    primer commit

--- public/js/app.js
+++ public/js/app.js
@@ -49427,8 +49427,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\OPLE\code\plantilla\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\OPLE\code\plantilla\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\OPLE\code\siadmon\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\OPLE\code\siadmon\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
