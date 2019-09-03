7a990f2c6180ce1c5a4cbe80e23ed16edb5b223b
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2019-09-03 17:57:58 +0000

    cors

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@ return [
    
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
-    'allowedOriginsPatterns' => ['*'],
+    'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
     'exposedHeaders' => [],
