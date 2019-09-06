5c264d95bcd6638c91f76d8466c9164b9a2b650f
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2019-09-06 17:59:14 +0000

    cors 0609 12:59

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'disablepreventback' => \App\Http\Middleware\DisablePreventBack::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
