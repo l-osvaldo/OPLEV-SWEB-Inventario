337e33b9c29860b7b4b8ef02b3224ac9b2e7f19b
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2019-09-10 18:42:30 +0000

    cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -13,13 +13,12 @@ class CORS {
      */
     public function handle($request, Closure $next)
     {
-        $prueba = array(1, 2, array("a", "b", "c"));
         header('Access-Control-Allow-Origin: *');
        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
        // hay que agregarle un die() aquí y subirlo para ver si esta entrando
-       var_dump($prueba);
-       die('cors');
+
+       //die('cors');
        return $next($request);
     }
 
