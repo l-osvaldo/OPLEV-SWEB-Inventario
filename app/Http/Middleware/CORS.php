0e7e90901388685ab65543a6a7c9e9b9dd52e6d9
creducindo <carlosreducindo@gmail.com>
2019-09-11 14:54:25 +0000

    pruebas cors die

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -14,14 +14,18 @@ class CORS {
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-       header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-       header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
-       // hay que agregarle un die() aquí y subirlo para ver si esta entrando
+        var_dump("antes de los headers");
 
-       //die('cors');
-       Log::notice("Valida");
-       return $next($request);
+
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
+        // hay que agregarle un die() aquí y subirlo para ver si esta entrando
+
+        var_dump("despues de los headers");
+        Log::notice("Antes del die");
+        die();       
+        return $next($request);
     }
 
 }
