3ab4d5abc75c2c5a21df8a5b69f3f961d012dab7
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2019-11-19 19:16:07 +0000

    revision

--- app/Http/Controllers/RevisionController.php
+++ app/Http/Controllers/RevisionController.php
@@ -19,6 +19,45 @@ class RevisionController extends Controller
 
         $cancelaciones = cancelacionresguardos::orderBy('nombreempleado', 'ASC')->get();
 
+        foreach ($cancelaciones as $cancelacion) {
+        	$numArticulosOPLE = bitacoracancelaciones::where([['id_cancelacion', $cancelacion->Id],['numeroinventario','like','%OPLEV%']])->orWhere('numeroinventario','like','%IEV%')->count();
+
+        	$numArticulosECO = bitacoracancelaciones::where([['id_cancelacion', $cancelacion->Id],['numeroinventario','like','%ECO%']])->count();
+
+        	array_add($cancelacion,'numArticulosOPLE',$numArticulosOPLE);
+        	array_add($cancelacion,'numArticulosECO',$numArticulosECO);
+
+        	$totalImporteOPLE = 0;
+        	$totalImporteECO = 0;
+
+        	if ($numArticulosOPLE > 0){
+        		$articulosOPLE = bitacoracancelaciones::where([['id_cancelacion', $cancelacion->Id],['numeroinventario','like','%OPLEV%']])->orWhere('numeroinventario','like','%IEV%')->get();
+
+        		foreach ($articulosOPLE as $articulo) {
+        			$importe = articulos::select('importe')->where('numeroinv',$articulo->numeroinventario)->get();
+
+        			$totalImporteOPLE += $importe[0]['importe'];
+        		}
+        		$totalImporteOPLE = number_format($totalImporteOPLE,2);
+        	}
+
+        	array_add($cancelacion,'totalImporteOPLE',$totalImporteOPLE);
+
+        	if ($numArticulosECO > 0){
+        		$articulosECO = bitacoracancelaciones::where([['id_cancelacion', $cancelacion->Id],['numeroinventario','like','%ECO%']])->get();
+
+        		foreach ($articulosECO as $articuloE) {
+        			$importe = articulosecos::select('importe')->where('numeroinventario',$articuloE->numeroinventario)->get();
+
+        			$totalImporteECO += $importe[0]['importe'];
+        		}
+        		$totalImporteECO = number_format($totalImporteECO,2);
+
+        	}
+
+        	array_add($cancelacion,'totalImporteECO',$totalImporteECO); 
+        }
+
         return view('revision.revision', compact('usuario','cancelaciones'));
     }
 }
--- resources/views/revision/revision.blade.php
+++ resources/views/revision/revision.blade.php
@@ -8,4 +8,60 @@
 
 	@include('sweet::alert')
 
+	<section class="content" style="margin-top: 2vh;">
+
+		<div class="row ">
+			    <div class="col-12">     
+			      <div class="center-block">	      	
+			        <div class="card">
+			          <div class="card-body" >
+			            <table id="tableCatalogo" name="tableCatalogo" class="table table-bordered table-striped" style="width:100%">
+			              <thead>
+			                <tr>
+			                  <th style="text-align: center">Número de cancelación</th>
+							  <th style="text-align: center">Nombre del empleado</th>
+							  <th style="text-align: center">Área</th>
+							  <th style="text-align: center">Número de artículos OPLE</th>
+							  <th style="text-align: center">Número de artículos ECO</th>
+							  <th style="text-align: center">Importe OPLE</th>
+							  <th style="text-align: center">Importe ECO</th>
+							  <th style="text-align: center">Acciones</th>
+			                </tr>
+			              </thead>
+			              	@foreach ($cancelaciones as $cancelacion)
+				                <tr>
+				                	<td>{{ $cancelacion->Id }}</td>
+						          	<td>{{ $cancelacion->nombreempleado }}</td>
+						          	<td>{{ $cancelacion->nombrearea }}</td>
+						          	<td>{{ $cancelacion->numArticulosOPLE}}</td>
+						          	<td>{{ $cancelacion->numArticulosECO}}</td>
+						          	<td>$ {{ $cancelacion->totalImporteOPLE}}</td>
+						          	<td>$ {{ $cancelacion->totalImporteECO}} </td>    
+						          	<td>
+						          		<div class="btn-group">
+										  <button class="btn btn-default btn-sm dropdown-toggle"
+										          type="button" data-toggle="dropdown">
+										    Acciones <span class="caret"></span>
+										  </button>
+										  <ul class="dropdown-menu" role="menu">
+										    <li><a href="#" style="margin-left: 10px; color: #EA0D94">Detalle OPLE</a></li>
+										    <li><a href="#" style="margin-left: 10px; color: #EA0D94">Detalle ECO</a></li>
+										    <li><a href="#" style="margin-left: 10px; color: #EA0D94">Reporte</a></li>
+										    <li><a href="#" style="margin-left: 10px; color: #EA0D94">Reasignar</a></li>
+										  </ul>
+										</div>
+						          	</td>          
+				                </tr>
+			                @endforeach
+			              <tbody>
+			                  
+			              </tbody>
+			            </table>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+			</div>
+	</section>
+
 @endsection
