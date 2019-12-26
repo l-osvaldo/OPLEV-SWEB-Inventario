dacd9c614f68ab7c9bb952af0cd880e6bba166da
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2019-12-26 20:54:31 +0000

    detalles

--- resources/views/eco/reportesECO.blade.php
+++ resources/views/eco/reportesECO.blade.php
@@ -63,12 +63,20 @@
 	</div>
 
 	<div id="cargandoECO" style="display: none;">
-		
-		<div class="d-flex justify-content-center" >
-		  <div class="spinner-border" role="status">
-		    <span class="sr-only">Loading...</span>
-		  </div>
+		<div id="loader" class="hidden">
+			<div class="cubes">
+				<div class="sk-cube sk-cube1"></div>
+				<div class="sk-cube sk-cube2"></div>
+				<div class="sk-cube sk-cube3"></div>
+				<div class="sk-cube sk-cube4"></div>
+				<div class="sk-cube sk-cube5"></div>
+				<div class="sk-cube sk-cube6"></div>
+				<div class="sk-cube sk-cube7"></div>
+				<div class="sk-cube sk-cube8"></div>
+				<div class="sk-cube sk-cube9"></div>
+			</div>
 		</div>
+		
 	</div>
 	<br>
 	<div class="col-12" id="divRespuestaECO" style="display: none;">     
